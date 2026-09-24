24941f9d728672bfac1f7f5579bba6379ae6c330
nik00did <02.nik.03.did.00@gmail.com>
2026-09-24 14:53:04 +0200

    added version mark

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,7 @@
 import "./style.css"
 import { ICONS } from "./constants.ts"
 import { initScene } from "./logic/initScene.ts"
+import packageJson from "../package.json" with { type: "json" }
 
 const root = document.documentElement
 
@@ -8,5 +9,13 @@ root.style.setProperty("--icon-start", `url("${ICONS.START}")`)
 root.style.setProperty("--icon-pause", `url("${ICONS.PAUSE}")`)
 root.style.setProperty("--icon-resume", `url("${ICONS.RESUME}")`)
 root.style.setProperty("--icon-restart", `url("${ICONS.RESTART}")`)
+
+const versionLabel = document.querySelector("[data-app-version]")
+
+if (versionLabel) {
+    const [major, minor] = packageJson.version.split(".")
+
+    versionLabel.textContent = `${major}.${minor}`
+}
 
 initScene()
